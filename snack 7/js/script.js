c8dbc3cf2c2f2eb58413a5ff04d0761df56075d0
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-14 14:43:28 +0200

    Completato da solo.

--- snack 7/js/script.js
+++ snack 7/js/script.js
@@ -2,12 +2,16 @@
 // Fai inserire un numero, che chiameremo N, all'utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
 
 
-let numeroUtente = Number.parseInt(prompt("Dammi un numero da uno a cinque!"))
+let numeroUtente = Number.parseInt(prompt("Dammi un numero!"))
 
-// for (let k = 0; k < numeroUtente; k++){
-//     const array = [];
-//     for(let i = 0; i < 10; i++){
-//         array.phus(Math.floor(Math.random() * 100) + 1)
-//     }
-// }
-console.log(array)
+if(numeroUtente > 20 || numeroUtente < 1 ||  isNaN(numeroUtente) ){
+    numeroUtente =12
+}
+
+for(let index = 0; index < numeroUtente; index++){
+    let numeriCasuali = [];
+    for(let index = 0; index < 10; index++){
+        numeriCasuali.push(Math.floor(Math.random() * 10) + 1)
+    }
+    console.log(numeriCasuali)
+}
