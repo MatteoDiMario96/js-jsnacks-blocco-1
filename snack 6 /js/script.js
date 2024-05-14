740c1310f4449c8e443a28be28a543206834b31b
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-14 10:43:22 +0200

    per adesso

---
+++ snack 6 /js/script.js
@@ -0,0 +1,18 @@
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+const array = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10
+]
+
+if (array.indexOf % 2 == 1) {
+    
+}
