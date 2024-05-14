fae2f5dbb908526c45ecc175d2c513ba3f475ce3
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-14 14:49:06 +0200

    snack 6 fatto da solo.

--- snack 6 /js/script.js
+++ snack 6 /js/script.js
@@ -1,22 +1,12 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
+const numeriInteri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
 let somma = 0;
 
-const array = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10
-// ]
-// for (let i = 1; i < array.length; i++){
-//     if (i % 2 == 1) {
-//     somma += array[i]
-//     }   
-// }
+for(let index = 0; index < numeriInteri.length; index++){
+    if(index % 2 === 1){
+        somma += numeriInteri[index]
+    }
+}
 console.log(somma)
